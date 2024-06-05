301bd38f8f78a180b7ff19910d37412a28d98f3a
Gauravgondaliya13 <135405545+Gauravgondaliya13@users.noreply.github.com>
2024-06-05 11:01:38 +0530

    Add files via upload

---
+++ Assignment All/Assignment Modual-7/style/jqury/jqury.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".gret").css({'border': '3px solid red',
+    'color': 'red', 'width':'98%', 'height':'330px','padding':'15px'})
+    $(".great").css({'border': '3px solid red',
+    'color': 'red', 'width':'95%', 'height':'300px','padding':'15px'})
+    $(".grandp").css({'border':'3px solid red','width':'50%', 'height':'250px' ,'margin-left':'10px', 'padding':'15px'})
+    $(".grandp1").css({'border':'3px solid red','width':'93%', 'height':'200px' ,'margin-left':'10px','padding':'15px'})
+    $(".grandp2").css({'border':'3px solid red','width':'90%', 'height':'160px' ,'margin-left':'10px','padding':'10px'})
+    $(".grandp3").css({'border':'1px solid grey','color':'grey',
+    'width':'50%', 'height':'28px' ,'margin-left':'10px','padding':'5px'})
+})
